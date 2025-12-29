aa02f1cf262a327c4c838ae0a7c5b90efd65b006
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-12-30 01:37:19 +0530

    .

---
+++ client-service-001/src/main/java/org/learn/client_service_001/ClientService001Application.java
@@ -0,0 +1,13 @@
+package org.learn.client_service_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientService001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientService001Application.class, args);
+	}
+
+}
---
+++ client-service-001/src/test/java/org/learn/client_service_001/ClientService001ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.client_service_001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientService001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
