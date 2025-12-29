b247e99f1786b07a84be3a9fc6ea1c3b8f238519
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-12-30 01:48:20 +0530

    .

--- client-service-001/src/main/java/org/learn/client_service_001/controller/Test.java
+++ client-service-001/src/main/java/org/learn/client_service_001/controller/Test.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
     @GetMapping("/test")
     public ResponseEntity<?> test() {
+        System.out.println("Testing...");
         return ResponseEntity.ok("Testing...");
     }
 }
