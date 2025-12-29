5da856f14272feb1d95117b8b1a644730a6c7bc5
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-12-30 01:42:30 +0530

    .

--- client-service-001/src/main/java/org/learn/client_service_001/ClientService001Application.java
+++ client-service-001/src/main/java/org/learn/client_service_001/ClientService001Application.java
@@ -3,11 +3,14 @@ package org.learn.client_service_001;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ClientService001Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClientService001Application.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(ClientService001Application.class, args);
+    }
 
 }
