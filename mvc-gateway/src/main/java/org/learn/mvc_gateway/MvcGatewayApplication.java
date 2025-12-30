2ed07bbdefe9952b8701cbb23b3a32a38a86f691
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-12-30 17:30:01 +0530

    .

---
+++ mvc-gateway/src/main/java/org/learn/mvc_gateway/MvcGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.learn.mvc_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcGatewayApplication.class, args);
+	}
+
+}
---
+++ mvc-gateway/src/test/java/org/learn/mvc_gateway/MvcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.mvc_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
